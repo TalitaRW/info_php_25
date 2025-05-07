c7672659fc5e5ebd795288b9e9949e361282f47f
suporte <suporte@BG-LABX-PCX>
2025-05-06 21:25:12 -0300

    Aula 02

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Olá Mundo <br>"; 
+//<br> quebra de linha html - navegador so entente HTML, Java
+//usa \n se for executar no terminal
+echo "Soma 2 + 2 = ", 2 + 2;
+
+/* comentario
+    de bloco*/
+
+$d = 3;
+$e = 5;
+$f = $d + $e;
+
+echo "<br> Soma d + e = ", $f;
+
